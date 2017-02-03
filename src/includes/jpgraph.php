b7a6b4620fdca870afe6994970ce6328c4975fd7
Andreas <iquito@gmx.net>
2017-02-03 17:32:10 +0100

    Remove setting error_reporting
    When using the Graph class error_reporting and display_errors get overwritten to E_ALL and true, which is very undesirable, especially for production. Removing this should change nothing when using JPGraph and only lead to a better experience. I suspect this was included to find a bug originally but then forgotten.

--- src/includes/jpgraph.php
+++ src/includes/jpgraph.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 //=======================================================================
 // File:        JPGRAPH.PHP
 // Description: PHP Graph Plotting library. Base module.
